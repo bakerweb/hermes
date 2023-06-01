258adf4f978ca290e0e396ca3422a188529cb574
Dallan Baker <dallan@bakerwebsolutions.com>
2023-06-01 12:44:59 -0600

    Added headers polyfill

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@
  * import {hermes} from 'hermes'
  * hermes.post('http://localhost:3001/api/v1/get-user, {data: {userId: 134}})
  */
+import { Headers } from 'headers-polyfill';
+
 export const hermes = {
   post,
   put,
